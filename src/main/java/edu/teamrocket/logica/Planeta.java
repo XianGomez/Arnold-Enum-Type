ca42822b220da2085e9070be5c4e9e374190d627
XianGomez <xiiangomezz@gmail.com>
2026-04-07 11:25:54 +0200

    feat(Planeta): Añadir constructor planeta con atributos masa radio getMasa getRadio

--- src/main/java/edu/teamrocket/logica/Planeta.java
+++ src/main/java/edu/teamrocket/logica/Planeta.java
@@ -1,6 +1,28 @@
 package edu.teamrocket.logica;
 
 public enum Planeta {
-    MERCURY, VENUS, EARTH, MARS,
-    JUPITER, SATURN, URANUS, NEPTUNE
+    MERCURY (3.303e+23, 2.4397e+6),
+    VENUS (4.869e+24, 6.0518e+6),
+    EARTH (5.976e+24, 6.37814e+6),
+    MARS (6.421e+23, 3.3972e+6),
+    JUPITER (1.9e+27,   7.1492e+7),
+    SATURN (5.688e+26, 6.0268e+7),
+    URANUS (8.686e+25, 2.5559e+7),
+    NEPTUNE (1.024e+26, 2.4746e+7);
+
+    private final double masa;
+    private final double radio;
+
+    Planeta(double masa, double radio) {
+        this.masa = masa;
+        this.radio = radio;
+    }
+
+    public double getMasa() {
+        return masa;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
 }
