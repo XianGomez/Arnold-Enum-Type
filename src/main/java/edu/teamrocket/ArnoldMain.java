1e7ec1a220c2d761c4b6d06264b6bb0b0919f699
XianGomez <xiiangomezz@gmail.com>
2026-04-09 09:56:14 +0200

    fix(ArnoldMain): Hacer que pase por el ArrayIndexOutOfBoundsException

--- src/main/java/edu/teamrocket/ArnoldMain.java
+++ src/main/java/edu/teamrocket/ArnoldMain.java
@@ -1,5 +1,6 @@
 package edu.teamrocket;
 
+import java.util.Arrays;
 import edu.teamrocket.logica.Planeta;
 
 public class ArnoldMain {
@@ -8,7 +9,7 @@ public class ArnoldMain {
 
         double peso = 1.0;
         
-        if (args.length == 2) {
+        if (args.length > 0) {
             try {
                 peso = Double.parseDouble(args[0]);
                 Planeta planeta = Planeta.valueOf(args[1].toUpperCase());
@@ -16,30 +17,32 @@ public class ArnoldMain {
                 return;
             } catch (IllegalArgumentException e) {
                 System.out.println("Check your parameters, it must be an integer and a valid planet name");
+                System.out.println("Valid planets names: " + Arrays.toString(Planeta.values()));
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("You have to provide a weight and a planet as argument");
             }
-            
+            return;
+        } 
 
-        } else {
-
-            for(Planeta planeta : Planeta.values()){
-                System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
-            }
-            
-            System.out.println("\nYour weight only on the terrestrial planets: ");
-            for(Planeta planeta: Planeta.getPlanetasTerrestres()){
-                System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
-            }
-            
-            System.out.println("\nYour weight only on the gas giant planets: ");
-            for(Planeta planeta: Planeta.getGigantesGaseosos()){
-                System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
-            }
-
-            System.out.println("\nYour weight only on the frozen planets: ");
-            for(Planeta planeta : Planeta.getPlanetasHelados()){
-                System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
-            }
+        for(Planeta planeta : Planeta.values()){
+            System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
         }
+            
+        System.out.println("\nYour weight only on the terrestrial planets: ");
+        for(Planeta planeta: Planeta.getPlanetasTerrestres()){
+            System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+        }
+            
+        System.out.println("\nYour weight only on the gas giant planets: ");
+        for(Planeta planeta: Planeta.getGigantesGaseosos()){
+            System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+        }
+
+        System.out.println("\nYour weight only on the frozen planets: ");
+        for(Planeta planeta : Planeta.getPlanetasHelados()){
+            System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+        }
+        
         
         
     }
